3df8bd620a7cf1e29b0494ab805ebde131004258
Daniel Blanco <danielblanconolasco@gmail.com>
2024-02-07 16:20:03 +0200

    cambio en el catch de getProducts e importación de fs/promises

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import fs from "fs"
+import fs from "fs/promises"
 
 let products = []
 
@@ -59,9 +59,8 @@ class ProductManager {
             const products = JSON.parse(data)
             return products
         } catch (error) {
-            console.error(`There was an error getting the products`, error);
-            await fs.writeFile(ProductManager.path, '[]', 'utf-8');
-            return [];
+            await fs.writeFile(ProductManager.path, '[]', 'utf-8')
+            return []
         }
     }
 
